03e42076b633a3c3a2e8587d564eb6e21c13646d
Shepherd <neiferjr15@gamil.com>
2023-05-02 10:46:03 -0400

    feat: add database connection

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
+
+	"github.com/BBCompanyca/Backend-BBCVentas.git/database"
+	"github.com/BBCompanyca/Backend-BBCVentas.git/settings"
 	"go.uber.org/fx"
 )
 
 func main() {
 
 	app := fx.New(
-		fx.Provide(),
+		fx.Provide(
+			context.Background,
+			settings.New,
+			database.New,
+		),
 		fx.Invoke(),
 	)
 
